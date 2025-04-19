8b66aa3e5642b455eae62a424699c8a505736104
Abdulrahman Jimoh <jimoh_abdol@yahoo.com>
2025-04-19 19:39:42 +0100

    added remove comment

--- internal/common/datasource.go
+++ internal/common/datasource.go
@@ -9,7 +9,6 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-// PGPASSWORD=7Nq5gF5hOXOrflibp8ugGVs1KDgRnaWa psql -h dpg-cvvfforuibrs73bdogkg-a.oregon-postgres.render.com -U test_mg7t_user test_mg7t
 func InitDB() (*gorm.DB, error) {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
