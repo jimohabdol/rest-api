1c9858c5ec438d6f3dfe78834f2715114e8f72cd
Abdulrahman Jimoh <jimoh_abdol@yahoo.com>
2025-04-21 23:47:22 +0100

    update routes

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/jimohabdol/rest-api/internal/booking"
 	"github.com/jimohabdol/rest-api/internal/common"
 	"github.com/jimohabdol/rest-api/internal/event"
+	"github.com/jimohabdol/rest-api/internal/router"
 	"github.com/jimohabdol/rest-api/internal/user"
 	"gorm.io/gorm"
 )
@@ -50,7 +51,11 @@ func main() {
 	bookingRepo := booking.NewRepository(db)
 	bookingService := booking.NewService(bookingRepo)
 	bookingHandler := booking.NewHandler(bookingService)
-	authService := auth.NewService(os.Getenv("JWT_ACCESS_SECRET"), os.Getenv("JWT_REFESH_SECRET"), userRepo)
+	authService := auth.NewService(
+		os.Getenv("JWT_ACCESS_SECRET"), 
+		os.Getenv("JWT_REFESH_SECRET"), 
+		userRepo,
+	)
 	authMiddleware := auth.NewMiddleware(authService)
 	authHandler := auth.NewHandler(userService, authService)
 
@@ -59,48 +64,20 @@ func main() {
 	contextPath := server.Group("/api/v1")
 
 	// Auth routes
-	contextPath.POST("/auth/register", authHandler.Register)
-	contextPath.POST("/auth/login", authHandler.Login)
-	contextPath.POST("/auth/refresh", authHandler.RefreshToken)
-
+	router.AuthRouter(contextPath, authHandler)
 	// User routes
-	userPath := contextPath.Group("/")
-	userPath.Use(authMiddleware.AuthRequired())
-	{
-		userPath.POST("user", userHandler.CreateUser)
-		userPath.GET("users", userHandler.GetAllUsers)
-		userPath.DELETE("user/:id", userHandler.DeleteUser)
-		userPath.GET("user/:id", userHandler.GetUserByID)
-		userPath.PUT("user/:id", userHandler.UpdateUser)
-		userPath.GET("user/email/:email", userHandler.GetUserByEmail)
-	}
-
+	router.UserRouter(contextPath, userHandler, authMiddleware)
 	// Event routes
-	eventPath := contextPath.Group("/")
-	eventPath.Use(authMiddleware.AuthRequired())
-	{
-		eventPath.POST("event", eventHandler.CreateEvent)
-		eventPath.GET("events", eventHandler.GetAllEvents)
-		eventPath.DELETE("event/:id", eventHandler.DeleteEvent)
-		eventPath.GET("event/:id", eventHandler.GetEventByID)
-		eventPath.PUT("event/:id", eventHandler.UpdateEvent)
-		eventPath.GET("event/date/:date", eventHandler.GetEventByDate)
-	}
-
+	router.EvenRouter(contextPath, eventHandler, authMiddleware)
 	// Booking routes
-	bookingPath := contextPath.Group("/")
-	bookingPath.Use(authMiddleware.AuthRequired())
-	{
-		bookingPath.POST("booking", bookingHandler.CreateBooking)
-		bookingPath.GET("bookings", bookingHandler.GetAllBookings)
-		bookingPath.DELETE("booking/:id", bookingHandler.DeleteBooking)
-		bookingPath.GET("booking/:id", bookingHandler.GetBookingByID)
-		bookingPath.PUT("booking/:id", bookingHandler.UpdateBooking)
-		bookingPath.GET("bookings/user/:user_id", bookingHandler.GetBookingsByUserID)
-		bookingPath.GET("bookings/event/:event_id", bookingHandler.GetBookingsByEventID)
-		bookingPath.GET("bookings/date/:date", bookingHandler.GetBookingsByDate)
-		//bookingPath.GET("bookings/status/:status", bookingHandler.GetBookingsByStatus)
-	}
+	router.BookingRouter(contextPath, bookingHandler, authMiddleware)
+	// Health check route
+	contextPath.GET("/healthz", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Server is running",
+			"code":    0,
+		})
+	})
 
 	server.Run(":8080")
 }
---
+++ internal/router/router.go
@@ -0,0 +1,61 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jimohabdol/rest-api/internal/auth"
+	"github.com/jimohabdol/rest-api/internal/booking"
+	"github.com/jimohabdol/rest-api/internal/event"
+	"github.com/jimohabdol/rest-api/internal/user"
+)
+
+func AuthRouter(r *gin.RouterGroup, h *auth.Handler) {
+	AuthRoutes := r.Group("/auth")
+	{
+		AuthRoutes.POST("/auth/register", h.Register)
+		AuthRoutes.POST("/auth/login", h.Login)
+		AuthRoutes.POST("/auth/refresh", h.RefreshToken)
+	}
+}
+
+func UserRouter(r *gin.RouterGroup, h *user.Handler, authMiddleWare *auth.Middleware) {
+	userRoutes := r.Group("/users")
+	userRoutes.Use(authMiddleWare.AuthRequired())
+	{
+		userRoutes.POST("", h.CreateUser)
+		userRoutes.GET("/:id", h.GetUserByID)
+		userRoutes.PUT("/:id", h.UpdateUser)
+		userRoutes.DELETE("/:id", h.DeleteUser)
+		userRoutes.GET("", h.GetAllUsers)
+		userRoutes.GET("/email/:email", h.GetUserByEmail)
+	}
+}
+
+func EvenRouter(r *gin.RouterGroup, h *event.Handler, authMiddleWare *auth.Middleware) {
+	eventRoutes := r.Group("/envents")
+	eventRoutes.Use(authMiddleWare.AuthRequired())
+	{
+		eventRoutes.POST("event", h.CreateEvent)
+		eventRoutes.GET("events", h.GetAllEvents)
+		eventRoutes.DELETE("event/:id", h.DeleteEvent)
+		eventRoutes.GET("event/:id", h.GetEventByID)
+		eventRoutes.PUT("event/:id", h.UpdateEvent)
+		eventRoutes.GET("event/date/:date", h.GetEventByDate)
+	}
+}
+
+func BookingRouter(r *gin.RouterGroup, h *booking.Handler, authMiddleWare *auth.Middleware) {
+	bookingRoutes := r.Group("/bookings")
+	bookingRoutes.Use(authMiddleWare.AuthRequired())
+	{
+		bookingRoutes.POST("booking", h.CreateBooking)
+		bookingRoutes.GET("bookings", h.GetAllBookings)
+		bookingRoutes.DELETE("booking/:id", h.DeleteBooking)
+		bookingRoutes.GET("booking/:id", h.GetBookingByID)
+		bookingRoutes.PUT("booking/:id", h.UpdateBooking)
+		bookingRoutes.GET("bookings/user/:user_id", h.GetBookingsByUserID)
+		bookingRoutes.GET("bookings/event/:event_id", h.GetBookingsByEventID)
+		bookingRoutes.GET("bookings/date/:date", h.GetBookingsByDate)
+		//bookingPath.GET("bookings/status/:status", h.GetBookingsByStatus)
+	}
+}
+
